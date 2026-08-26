a50d2bf6c9ab352488ca231b564ea56f52929934
mojtaba shabani <mojtaba.shabani2580@gmail.com>
2026-08-26 20:02:21 +0330

    number of enmad changed

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       }}
     >
       <head>
-        <meta name="enamad" content="17330529" />
+        <meta name="enamad" content="61288267" />
       </head>
 
       <body className="min-h-screen  bg-slate-100 text-slate-950 antialiased">
